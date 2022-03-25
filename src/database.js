a77733c01186fa2bc29e04b89493de41541c3b71
SolohagaLukas <solohagalukas@gmail.com>
2022-03-25 17:14:44 -0300

    Backend with Nodejs, Express and Mongodb

---
+++ src/database.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const URI = 'mongodb://localhost/mern-tasks';
+
+mongoose.connect(URI)
+    .then(db => console.log('DB is connected'))
+    .catch(err => console.log(err));
+
+module.exports = mongoose;
